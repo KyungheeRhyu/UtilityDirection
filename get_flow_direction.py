c3143de8dc7c6b2aedce6a4b61ab3eb370fc2a34
Nick Libassi <nlibassi@cityoflewisville.com>
2025-07-14 12:01:39 -0500

    move set_environment() to separate file

--- get_flow_direction.py
+++ get_flow_direction.py
@@ -1,19 +1,7 @@
 import os
 import math
-from dotenv import load_dotenv
-import pathlib
 import arcpy
-
-
-def set_environment():
-    """
-    Set the environment for the script by loading the .env file and defining arcpy env settings. Assumes the .env file is in the same directory as this script.
-    """
-    script_dir = pathlib.Path(__file__).parent.resolve()
-    env_path = script_dir / '.env'
-    load_dotenv(dotenv_path=env_path)
-    arcpy.env.overwriteOutput = True
-    arcpy.env.workspace = os.getenv('GDB')  # Set the workspace from the .env file
+from shared import set_environment
 
 
 def get_projected_feature_class(in_fc, out_fc, out_coordinate_system, geographic_transformation):
