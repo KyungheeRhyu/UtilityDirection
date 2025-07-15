7141c4575aaa1e65c7f138f1b6b858fae854977c
Nick Libassi <nlibassi@cityoflewisville.com>
2025-07-15 14:17:58 -0500

    change search_distance to None when selecting points that intersect each segment; log duration

--- get_segment_details.py
+++ get_segment_details.py
@@ -1,4 +1,5 @@
 import os
+import datetime as dt
 import arcpy
 from shared import set_environment
 
@@ -12,20 +13,22 @@ def get_connected_point_count(line_layer, point_layer):
     new_field = 'connected_point_count'
     if new_field not in line_fields:
         arcpy.AddField_management(line_layer, new_field, 'SHORT')
+        print(f"Added new field '{new_field}' to line layer.")
     with arcpy.da.UpdateCursor(line_layer, ['OBJECTID', new_field]) as cursor:
+        print(f"Updating '{new_field}' field in line layer with connected point counts...")
         for row in cursor:
             arcpy.management.SelectLayerByAttribute(line_layer, 'NEW_SELECTION', f"OBJECTID = {row[0]}")
-            print(f"Selected line segment with OBJECTID: {row[0]}")
+            #print(f"Selected line segment with OBJECTID: {row[0]}")
             # Select points that intersect with the line segment - TODO - adjust search distance if necessary
             arcpy.management.SelectLayerByLocation(
                 in_layer=point_layer,
                 overlap_type="INTERSECT",
                 select_features=line_layer,
-                search_distance="0.05 Meters",
+                search_distance=None,
                 selection_type="NEW_SELECTION"
             )
             selected_point_count = arcpy.management.GetCount(point_layer)[0]
-            print(f"Selected points overlapping with line segment: {selected_point_count}")
+            #print(f"Selected points overlapping with line segment: {selected_point_count}")
             #rint(f"Selected line segments: {arcpy.management.GetCount(line_layer)}\n")
             row[1] = int(selected_point_count)
             cursor.updateRow(row)
@@ -35,12 +38,18 @@ def run():
     """
     Main function to run the script.
     """
+    start_time = dt.datetime.now()
+    print(f"Script started at {start_time}")
     set_environment()
     line_fc = os.getenv('INPUT_FC')
-    point_fc = os.getenv('MANHOLE_FC')
+    point_fc = os.getenv('POINT_FC')
     line_layer = arcpy.MakeFeatureLayer_management(line_fc, "line_layer")
     point_layer = arcpy.MakeFeatureLayer_management(point_fc, "point_layer")
     get_connected_point_count(line_layer, point_layer)
+    end_time = dt.datetime.now()
+    duration = end_time - start_time
+    print(f"Script ended at {end_time}")
+    print(f"Total duration: {duration}")
 
 
 if __name__ == "__main__":
